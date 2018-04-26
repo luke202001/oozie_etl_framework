9162a1fc048cbbb236da920239e67579ca50182d
ylai02 <yong.lai@bmo.com>
2018-04-26 13:29:44 -0400

    Minor adjust

--- oozie-client/src/main/java/com/altarmoss/oozie/client/OozieWorkUnit.java
+++ oozie-client/src/main/java/com/altarmoss/oozie/client/OozieWorkUnit.java
@@ -4,43 +4,114 @@ import java.text.SimpleDateFormat;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.altarmoss.oozie.action.InitialWorkflowAction;
+import com.altarmoss.oozie.client.utils.HiveConfig;
 import com.altarmoss.oozie.client.utils.WorkflowConfiguration;
+import com.altarmoss.oozie.database.IConfigDataHandler;
 
+@ComponentScan
 public class OozieWorkUnit {
 
 	private static Logger logger = LoggerFactory.getLogger(InitialWorkflowAction.class);
-	
 	private static String KEY_FILE = "keyFile.txt";
-	
 	protected static final SimpleDateFormat DF_yyyyMMddHHmmss = new SimpleDateFormat("yyyyMMddHHmmss");
 	
+	private IConfigDataHandler dataHandler;
+	@Autowired
+	private HiveConfig hiveConfig;
+	
 	public void execute(WorkflowConfiguration conf) {
-		
+		initDataHandler();
+//		getHiveHandler();
+//		loadWorkUnitProperties();
+//		getWorkUnit();
+//		executeWorkUnit();
+//		executeProp0();
+//		executeSource();
+//		executeEmailNotification();
+//		executeHsql();
 	}
 	
 	private static WorkflowConfiguration parseParameters(String[] args) {
 		if (args.length < 3) {
 			String errorMessage = "Incorrect number of parameters. " +
-								  "Usage: [Work Unit Name]";
+								  "Usage: [Work Unit Name]" +
+								  " -u [Username]" +
+								  " -keyfile [keyFile]" +
+								  " -level [Target Data Level (LEVEL1, LEVEL2, LEVEL3)" +
+								  " -group [Work Group name]" +
+								  " -enable [Enabled (Y or N)), default Y]" +
+								  " -hsql [HSQL File]" +
+								  " -prop [Property File]" +
+								  " -src [Source File]" +
+								  " -frameworkDb [Framework Database]" +
+								  " -emailnotif [Email Notification File]";
 			throw new RuntimeException(errorMessage);
 		}
 		WorkflowConfiguration result = new WorkflowConfiguration();
 		result.setWorkUnitName(args[0].toUpperCase());
 		
 		for (int i = 1; i < args.length; i++) {
-			if (args[i].equals("-u")) {
-				i++;
-				String[] userTokens = args[i].split(":");
-				result.setUserName(userTokens[0]);
-				if (userTokens.length > 1) {
-					result.setPassword(userTokens[1]);
-				}
+			switch (args[i]) {
+				case "-u":
+					String[] userTokens = args[++i].split(":");
+					result.setUserName(userTokens[0]);
+					if (userTokens.length > 1) {
+						result.setPassword(userTokens[1]);
+					}
+					break;
+				case "-level":
+					String targetLevel = args[++i];
+					switch (targetLevel) {
+					case "LEVEL1":
+						result.setTargetLevel("HDW_L1");
+						break;
+					case "LEVEL2":
+						result.setTargetLevel("HDW_L2");
+						break;
+					case "LEVEL3":
+						result.setTargetLevel("HDW_L3");
+						break;
+					default:
+						throw new RuntimeException("Invalid target data level: " + targetLevel + ".");
+					}
+					break;
+				case "-group":
+					result.setWorkGroupName(args[++i]);
+					break;
+				case "-enable":
+					result.setEnabled(args[++i]);
+					if (!result.getEnabled().equals("Y") &&
+						!result.getEnabled().equals("N")) {
+						throw new RuntimeException("Invalid enable value: " + result.getEnabled() + ".");
+					}
+					break;
+				case "-hsql":
+					String[] hsqlFiles = args[++i].split(",");
+					result.setHsqlFiles(hsqlFiles);
+					break;
+				case "-prop":
+					result.setPropFile(args[++i]);
+					break;
+				case "-src":
+					result.setSrcFile(args[++i]);
+					break;
+				case "-emailnotif":
+					result.setEmailNotificationFile(args[++i]);
+					break;
+				case "-frameworkDb":
+					result.setFrameworkDB(args[++i]);
+					break;
 			}
 		}
 		
 		return result;
+	}
+	
+	private void initDataHandler() {
 	}
 	
 	public static void main(String[] args) throws Exception {
--- oozie-client/src/test/java/com/altarmoss/oozie/client/OozieWorkUnitTest.java
+++ oozie-client/src/test/java/com/altarmoss/oozie/client/OozieWorkUnitTest.java
@@ -1,5 +1,6 @@
 package com.altarmoss.oozie.client;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
@@ -10,13 +11,22 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.altarmoss.oozie.client.utils.HiveConfig;
 import com.altarmoss.oozie.client.utils.TestUtil;
 import com.altarmoss.oozie.client.utils.WorkflowConfiguration;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes= {AppConfig.class})
 public class OozieWorkUnitTest {
 	private OozieWorkUnit oozieWorkUnit;
 	private Method parseParameters;
+	@Autowired
+	private HiveConfig hiveConfig;
 	
 	@Rule
 	public ExpectedException expectedEx = ExpectedException.none();
@@ -27,6 +37,7 @@ public class OozieWorkUnitTest {
 		parseParameters = TestUtil.getMethod(OozieWorkUnit.class,
 										   "parseParameters",
 										   String[].class);
+		System.out.println(hiveConfig.getDbUrl());
 	}
 
 	@Test
@@ -62,17 +73,82 @@ public class OozieWorkUnitTest {
 	}
 	
 	@Test
+	public void testParseParametersWithDifferentTargetLevels() 
+			throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+		
+		String args = "myWorkUnit -u testUser -level LEVEL1";
+		WorkflowConfiguration conf = (WorkflowConfiguration)parseParameters.invoke(oozieWorkUnit, new Object[] {args.split(" ")});
+		assertEquals("HDW_L1", conf.getTargetLevel());
+		
+		args = "myWorkUnit -u testUser -level LEVEL2";
+		conf = (WorkflowConfiguration)parseParameters.invoke(oozieWorkUnit, new Object[] {args.split(" ")});
+		assertEquals("HDW_L2", conf.getTargetLevel());
+		
+		args = "myWorkUnit -u testUser -level LEVEL3";
+		conf = (WorkflowConfiguration)parseParameters.invoke(oozieWorkUnit, new Object[] {args.split(" ")});
+		assertEquals("HDW_L3", conf.getTargetLevel());
+	}
+	
+	@Test
+	public void testParseParametersWithInvalidTargetLevel() 
+			throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+		expectedEx.expect(InvocationTargetException.class);
+
+		String args = "myWorkUnit -u testUser -level LEVEL4";
+		parseParameters.invoke(oozieWorkUnit, new Object[] {args.split(" ")});
+	}
+	
+	@Test
+	public void testParseParametersWithDifferentEnableValues() 
+			throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+		
+		String args = "myWorkUnit -u testUser -enable Y";
+		WorkflowConfiguration conf = (WorkflowConfiguration)parseParameters.invoke(oozieWorkUnit, new Object[] {args.split(" ")});
+		assertEquals("Y", conf.getEnabled());
+		
+		args = "myWorkUnit -u testUser -enable N";
+		conf = (WorkflowConfiguration)parseParameters.invoke(oozieWorkUnit, new Object[] {args.split(" ")});
+		assertEquals("N", conf.getEnabled());
+	}
+	
+	@Test
+	public void testParseParametersWithInvalidEnableValue() 
+			throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+		expectedEx.expect(InvocationTargetException.class);
+
+		String args = "myWorkUnit -u testUser -enable O";
+		parseParameters.invoke(oozieWorkUnit, new Object[] {args.split(" ")});
+	}
+	
+	@Test
 	public void testParseParametersFull() 
 			throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 		
 		String args = "myWorkUnit -u testUser " + 
 					  "-level LEVEL1 " + 
-					  "-hsql /landing_zone/staging/scripts/oozie/samples/test.sql " + 
-					  "-prop /configure/samples/job_properties";
+					  "-group TestGroup " +
+					  "-enable yes " +
+					  "-hsql /landing_zone/staging/scripts/oozie/samples/test.sql,test2.sql " + 
+					  "-prop /configure/samples/job_properties " +
+					  "-src mySrc " +
+					  "-emailnotif myEmail " +
+					  "-frameworkDb myFrameworkDB";
 		WorkflowConfiguration conf = (WorkflowConfiguration)parseParameters.invoke(oozieWorkUnit, new Object[] {args.split(" ")});
-		assertEquals(conf.getWorkUnitName(), "MYWORKUNIT");
-		assertEquals(conf.getUserName(), "testUser");
+		assertEquals("MYWORKUNIT", conf.getWorkUnitName());
+		assertEquals("testUser", conf.getUserName());
 		assertNull(conf.getPassword());
-
+		assertEquals("HDW_L1", conf.getTargetLevel());
+		assertEquals("TestGroup", conf.getWorkGroupName());
+		assertEquals("yes", conf.getEnabled());
+		assertArrayEquals(new String[] {"/landing_zone/staging/scripts/oozie/samples/test.sql", "test2.sql"}, conf.getHsqlFiles());
+		assertEquals("/configure/samples/job_properties", conf.getPropFile());
+		assertEquals("mySrc", conf.getSrcFile());
+		assertEquals("myEmail", conf.getEmailNotificationFile());
+		assertEquals("myFrameworkDB", conf.getFrameworkDB());
+	}
+	
+	@Test
+	public void testExecute() {
+		
 	}
 }
